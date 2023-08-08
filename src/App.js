534f522c2f8962bccd3915c06ae9a0b5e2bb4ba4
franc <fmenendezwork@gmail.com>
2023-08-08 00:36:16 -0700

    assignent complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import "./App.css";
+import TopBar from "./components/TopBar";
+import BackgroundImage from "./components/BackgroundImage";
+import StoreListings from "./components/StoreListings";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <TopBar />
+      <BackgroundImage />
+      <StoreListings />
     </div>
   );
 }
